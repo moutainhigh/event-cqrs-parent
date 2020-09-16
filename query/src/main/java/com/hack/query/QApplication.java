d84e881b01b28948dab7e5b67664817eb2690f4e
yuanjie111 <284522843@qq.com>
2020-09-16 10:34:34 +0800

    q 端

--- query/src/main/java/com/hack/query/QApplication.java
+++ query/src/main/java/com/hack/query/QApplication.java
@@ -1,4 +1,11 @@
 package com.hack.query;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class QApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QApplication.class,args);
+    }
 }
--- query/src/main/java/com/hack/query/listener/TradeLisener.java
+++ query/src/main/java/com/hack/query/listener/TradeLisener.java
@@ -1,5 +1,7 @@
 package com.hack.query.listener;
 
-@
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TradeLisener {
 }

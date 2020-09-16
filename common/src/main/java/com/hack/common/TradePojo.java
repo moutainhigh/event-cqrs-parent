b53cd9576b7920f8130199982cf2e1ddf4627873
yuanjie111 <284522843@qq.com>
2020-09-16 14:24:40 +0800

    q and c

--- common/src/main/java/com/hack/common/TradePojo.java
+++ common/src/main/java/com/hack/common/TradePojo.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class TradePojo {
 
     private long tradeId;
@@ -15,4 +13,45 @@ public class TradePojo {
     private String trader;
     private String salesPerson;
 
+    public TradePojo() {
+    }
+
+    public TradePojo(long tradeId, String cusip, String trader, String salesPerson) {
+        this.tradeId = tradeId;
+        this.cusip = cusip;
+        this.trader = trader;
+        this.salesPerson = salesPerson;
+    }
+
+    public long getTradeId() {
+        return tradeId;
+    }
+
+    public void setTradeId(long tradeId) {
+        this.tradeId = tradeId;
+    }
+
+    public String getCusip() {
+        return cusip;
+    }
+
+    public void setCusip(String cusip) {
+        this.cusip = cusip;
+    }
+
+    public String getTrader() {
+        return trader;
+    }
+
+    public void setTrader(String trader) {
+        this.trader = trader;
+    }
+
+    public String getSalesPerson() {
+        return salesPerson;
+    }
+
+    public void setSalesPerson(String salesPerson) {
+        this.salesPerson = salesPerson;
+    }
 }
--- query/src/main/java/com/hack/query/listener/TradeListener.java
+++ query/src/main/java/com/hack/query/listener/TradeListener.java
@@ -1,7 +1,30 @@
 package com.hack.query.listener;
 
+import com.hack.common.TradePojo;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class TradeListener {
+
+    private Map<Long,TradePojo> cache=new ConcurrentHashMap();
+
+    /**
+     * mock kafka 的监听
+     */
+    @RequestMapping("onEvent")
+    public void onEvent(TradePojo tradePojo){
+        cache.put(tradePojo.getTradeId(),tradePojo);
+    }
+
+    @RequestMapping("find/{tradeId}")
+    public Mono<TradePojo> findById(@PathVariable long tradeId){
+        return Mono.just(cache.get(tradeId));
+
+    }
 }

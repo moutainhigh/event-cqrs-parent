3ca17fc40f888fb85b0ae399f0c82572edade1ce
liwei <670415962@qq.com>
2020-09-16 20:58:29 +0800

    trade service

--- command/src/main/java/com/hack/web/trade/domain/service/TradeService.java
+++ command/src/main/java/com/hack/web/trade/domain/service/TradeService.java
@@ -1,5 +1,6 @@
 package com.hack.web.trade.domain.service;
 
+import com.hack.common.TradeCreateEvent;
 import com.hack.core.event.DataFlowEvent;
 import com.hack.core.event.EventMultiCaster;
 import com.hack.web.trade.command.CreateTradeCommand;
@@ -9,6 +10,7 @@ import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,17 +26,24 @@ public class TradeService {
     @Autowired
     private EventMultiCaster eventMultiCaster;
 
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
     public TradeEntity create(CreateTradeCommand createTradeCommand) {
         TradeEntity tradeEntity = new TradeEntity();
         BeanUtils.copyProperties(createTradeCommand, tradeEntity);
         tradeRepository.save(tradeEntity);
+        //异步执行
         eventMultiCaster.asyncMulticastFlowEvent(DataFlowEvent.<TradeEntity>builder()
+                .data(tradeEntity)
                 .subscribe(event -> refDataService.callRefData())
-                .onCompleted(event->{
-                    // 这里发送事件
-                }).onError((event,e)->{
-                     log.error("create trade error",e);
-                 }).build());
+                .onCompleted(event -> {
+                    // 这里发送spring 事件
+                    TradeCreateEvent tradeCreateEvent = new TradeCreateEvent();
+                    BeanUtils.copyProperties(event, tradeCreateEvent);
+                    applicationEventPublisher.publishEvent(tradeCreateEvent);})
+                .onError((event, e) -> log.error("create trade error", e))
+                .build());
         return tradeEntity;
     }
 }
